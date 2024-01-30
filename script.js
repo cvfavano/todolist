c35532873d1adcd53b9f2a632f731ffc1d37c314
Christa Favano <crstav@gmail.com>
2024-01-29 21:49:17 -0500

    Add form validation and clear form functions

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ const storageModel = (() => {
 
 var displayController =  (() => {
 
-    let toggleButton = document.querySelectorAll('.toggle');
-    let isDisplayType;
     let formContainer;
+    let toggleButton = document.querySelectorAll('.toggle');
+    let submitButton = document.querySelector('.submit');
 
     function getModalState(element) {
             if (element == 'block') 
@@ -50,24 +50,82 @@ var displayController =  (() => {
             else return false
     }
 
-    function toggle() {
-        let isDisplay = getModalState(isDisplayType);
+    function toggle(element, displayType) {
+        let isDisplay = getModalState(displayType);
     
         if(!isDisplay ) 
-            formContainer.style.display = 'block';    
+        element.style.display = 'block';    
     
         else 
-            formContainer.style.display = 'none'; 
+        element.style.display = 'none'; 
+    }
+
+
+    function hideAllErrorValidation() {
+        document.getElementById('validation-error-title').style.display = 'none';
+        document.getElementById('validation-error-description').style.display = 'none';
+        document.getElementById('validation-error-dueDate').style.display = 'none';
+
+    
+    }
+    let allFormInput = ['title','description', 'dueDate', 'notes'];
+    function clearAllInputValues(formElements){
+
+        formElements.forEach(element => {
+            document.getElementById(element).value = '';
+        })
+       
+
+    }
+    function checkValue(element) {
+       
+        let currentElement = document.querySelector(`#${element}`);
+
+        if(currentElement.value == "") {
+            //display validation
+            
+            let errorContainer = document.getElementById(`validation-error-${element}`);
+
+            let isDisplayType = window.getComputedStyle(errorContainer).display;
+            toggle(errorContainer,isDisplayType);
+            return false;
+        }
+
+        else {
+            return true;
+            //add tp storage
+            console.log({current: currentElement.value})
+            //close form
+            
+        }
     }
 
     toggleButton.forEach((button) => button.addEventListener('click', () => {
 
         formContainer = document.getElementById('add-item');
-        isDisplayType = window.getComputedStyle(formContainer).display;
-        toggle();
+        let isDisplayType = window.getComputedStyle(formContainer).display;
+        toggle(formContainer, isDisplayType);
    
     }));
     
+    submitButton.addEventListener('click', () => {
+        //check values for empties
+        let titleCheck = checkValue('title');
+        let descCheck = checkValue('description');
+        let dateCheck = checkValue('dueDate');
+
+        if (titleCheck && descCheck && dateCheck) {
+            hideAllErrorValidation();
+
+            clearAllInputValues(allFormInput);
+
+            let isDisplayType = window.getComputedStyle(formContainer).display;
+            toggle(formContainer, isDisplayType);
+
+        }
+
+        // get item and sent to storage
+    });
       
     return {
      
