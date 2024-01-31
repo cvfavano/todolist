e351de59154d953ed9b99f16b445f2b6c26d6172
Christa Favano <crstav@gmail.com>
2024-01-30 21:08:02 -0500

    Correct validation functions to display errors properly

--- script.js
+++ script.js
@@ -59,23 +59,25 @@ var displayController =  (() => {
         else 
         element.style.display = 'none'; 
     }
-
-
+    function displayError(element){
+        document.getElementById(`validation-error-${element}`).style.display = 'block';
+    }
+    function hideError(element){
+        document.getElementById(`validation-error-${element}`).style.display = 'none';
+    }
     function hideAllErrorValidation() {
+          //maybe foreach through the array
         document.getElementById('validation-error-title').style.display = 'none';
         document.getElementById('validation-error-description').style.display = 'none';
         document.getElementById('validation-error-dueDate').style.display = 'none';
-
-    
     }
+
     let allFormInput = ['title','description', 'dueDate', 'notes'];
     function clearAllInputValues(formElements){
 
         formElements.forEach(element => {
             document.getElementById(element).value = '';
         })
-       
-
     }
     function checkValue(element) {
        
@@ -83,21 +85,50 @@ var displayController =  (() => {
 
         if(currentElement.value == "") {
             //display validation
-            
-            let errorContainer = document.getElementById(`validation-error-${element}`);
-
-            let isDisplayType = window.getComputedStyle(errorContainer).display;
-            toggle(errorContainer,isDisplayType);
             return false;
         }
 
         else {
             return true;
-            //add tp storage
-            console.log({current: currentElement.value})
-            //close form
+            
             
         }
+    }
+
+    function validateForm() {
+
+        //maybe foreach through the array
+        let titleCheck = checkValue('title');
+        let descCheck = checkValue('description');
+        let dateCheck = checkValue('dueDate');
+
+        if(!titleCheck ) {
+            displayError('title');
+        }
+        if(!descCheck ) {
+            displayError('description');
+        }
+        if(!dateCheck ) {
+            displayError('dueDate');
+        }
+        if(titleCheck)
+            hideError('title');
+
+        if(descCheck)
+            hideError('description')
+        if(dateCheck)
+            hideError('dueDate')
+
+        if (titleCheck && descCheck && dateCheck) {
+            hideAllErrorValidation();
+
+            clearAllInputValues(allFormInput);
+
+            let isDisplayType = window.getComputedStyle(formContainer).display;
+            toggle(formContainer, isDisplayType);
+
+        }
+
     }
 
     toggleButton.forEach((button) => button.addEventListener('click', () => {
@@ -110,20 +141,9 @@ var displayController =  (() => {
     
     submitButton.addEventListener('click', () => {
         //check values for empties
-        let titleCheck = checkValue('title');
-        let descCheck = checkValue('description');
-        let dateCheck = checkValue('dueDate');
 
-        if (titleCheck && descCheck && dateCheck) {
-            hideAllErrorValidation();
-
-            clearAllInputValues(allFormInput);
-
-            let isDisplayType = window.getComputedStyle(formContainer).display;
-            toggle(formContainer, isDisplayType);
-
-        }
-
+        validateForm();
+       
         // get item and sent to storage
     });
       
